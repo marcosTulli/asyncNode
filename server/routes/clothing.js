35333e5835ddbd44721ab8f945f838ad798d5e44
Marcos Tulli <marcos.tulli@raona.com>
2023-08-13 22:24:54 -0300

    Refactor clothing.js with callback

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -4,14 +4,27 @@ const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+router.route('/').get(function (req, res) {
+  getClothingData((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Getting clothing data');
+      res.send(data);
+    }
   });
+  console.log('Doing more work Work');
+});
+
+function getClothingData(callback) {
+  fs.readFile(datafile, 'utf8', (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let clothingData = JSON.parse(data);
+      callback(null, clothingData);
+    }
+  });
+}
 
 module.exports = router;
