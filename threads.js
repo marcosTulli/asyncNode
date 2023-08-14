fe337d68f3ea54913878fb1af7e78d3f7febf837
Marcos Tulli <marcos.tulli@raona.com>
2023-08-14 11:40:47 -0300

    Approach to multi threads

---
+++ threads.js
@@ -0,0 +1,31 @@
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+if (isMainThread) {
+  console.log('Starting the main thread.');
+  const worker = new Worker(__filename, {
+    workerData: {
+      outputPrefix: 'Received message',
+      timeToWaste: 500,
+    },
+  });
+  worker.on('message', (msg) => {
+    console.log(`Worker msg: ${msg}`);
+  });
+
+  worker.postMessage('Done with my Work');
+  console.log('Still in the main thread');
+} else {
+  parentPort.on('message', (msg) => {
+    console.log(`${workerData.outputPrefix}: ${msg}`);
+  });
+  parentPort.postMessage('Getting started');
+  wasteTime(workerData.timeToWaste);
+  parentPort.postMessage('In the middle');
+  wasteTime(workerData.timeToWaste);
+  parentPort.postMessage('All Done');
+}
+
+function wasteTime(delay) {
+  const end = Date.now() + delay;
+  while (Date.now() < end) {}
+}
